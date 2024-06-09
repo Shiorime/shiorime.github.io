af19f9a4a9bcfdda83968a618f2c5ade619e282a
Shizukanawa <deviousfinest@gmail.com>
2024-06-09 02:18:33 +0200

    Start working on todo page

--- src/Pages/ToDo.js
+++ src/Pages/ToDo.js
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function ToDo() {
   return (
-    <h1>To Do</h1>
+    <>
+        <h1>To Do</h1>
+        <button>Add To-Do</button>
+        <button>Clear marked To-Dos</button>
+    </>
   )
 }
