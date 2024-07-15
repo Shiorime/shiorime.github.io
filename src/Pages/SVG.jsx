72f0ada61224325eb9a4e6214cbf7466d654bffc
Shizukanawa <deviousfinest@gmail.com>
2024-07-16 01:21:01 +0200

    Added an svg image

--- src/Pages/SVG.jsx
+++ src/Pages/SVG.jsx
@@ -3,8 +3,14 @@ import React from 'react'
 export default function SVG() {
   return (
     <>
-      <svg>
-
+      <svg width="800" height="600" xmlns="http://www.w3.org/2000/svg">
+        <g>
+          <title>Layer 1</title>
+          <ellipse ry="130.5" rx="137" id="svg_1" cy="306.5" cx="387" stroke="#000" fill="#fff" />
+          <ellipse ry="130.5" rx="137" id="svg_3" cy="306.5" cx="387" stroke="#000" fill="#fff" />
+          <ellipse ry="130.5" rx="137" id="svg_4" cy="306.5" cx="387" stroke="#000" fill="#fff" />
+          <ellipse ry="130.5" rx="137" id="svg_5" cy="306.5" cx="387" stroke="#000" fill="#fff" />
+        </g>
       </svg>
       <div>Test</div>
     </>
