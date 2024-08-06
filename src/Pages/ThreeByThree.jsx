eb927017179165508bdd5843b4c177acd49cd9bd
Shizukanawa <deviousfinest@gmail.com>
2024-08-06 02:05:16 +0200

    Added an anime title and styling to it

--- src/Pages/ThreeByThree.jsx
+++ src/Pages/ThreeByThree.jsx
@@ -2,15 +2,21 @@ import React, { useState } from 'react'
 import "./styles.css"
 
 const images = ["https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx20698-YZIYor2zW3Ta.png",
-                "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx21455-hTd6T0s9fvYj.jpg"]
+                "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx21455-hTd6T0s9fvYj.jpg"
+]
 const backgroundPositions = ["30% 50%", 
-                             "55% 35%"];
+                             "55% 35%"
+];
+const animeTitles = ["Yahari Ore no Seishun Love Come wa Machigatteiru.",
+                     "NEW GAME!"
+]
 
 export default function ThreeByThree() {
   const [currentNumber, setCurrentNumber] = useState(0);
   const [image, setImage] = useState(images[0]);
   const [imagePosition, setImagePosition] = useState(backgroundPositions[0]);
   const [opacity, setOpacity] = useState(1);
+  const [animeTitle, setAnimeTitle] = useState(animeTitles[0]);
 
   const setBigDisplay = (number) => {
     if (number != currentNumber) {
@@ -19,6 +25,7 @@ export default function ThreeByThree() {
       setTimeout(() => {
         setImage(images[number]);
         setImagePosition(backgroundPositions[number]);
+        setAnimeTitle(animeTitles[number]);
         setOpacity(1); // Start the transition to fade in
       }, 200); // This timeout duration should match the CSS transition duration
     }
@@ -47,6 +54,9 @@ export default function ThreeByThree() {
               opacity: opacity
             }}
           ></div>
+        </div>
+        <div className="anime-title-wrapper">
+          <div className="anime-title" style={{ opacity: opacity }}>{animeTitle}</div>
         </div>
       </div>
     </>
